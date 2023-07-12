71d70a03688b004aad957ece5597c6114e8fafbc
Jerry99699 <130133166+Jerry99699@users.noreply.github.com>
2023-07-12 12:50:26 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ seconds = minutes * 60
 
 while seconds:
     mins, secs = divmod(seconds, 60)
-    timer = '{:02d}:{:02d}'。format(mins, secs)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
     print(timer, end="\r")
     time.sleep(1)
     seconds -= 1
